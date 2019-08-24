c703c2a5ab9ca5bee5e000ef9bbedffebbc1d5b5
Grzegorz Motyl <grzegorz.motyl@stepstone.com>
2019-08-24 12:25:55 +0200

    #2 fetch intentions from api - hardcoded intentions fetch from mock api - axios dependency added

--- src/containers/IntentionList/IntentionList.tsx
+++ src/containers/IntentionList/IntentionList.tsx
@@ -1,7 +1,10 @@
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
+import axios from 'axios';
 import * as React from "react";
+import { useEffect, useState } from 'react';
+import { IIntention } from 'src/components/Intention/Interface';
 import Intention from "../../components/Intention";
 
 const useStyles = makeStyles(theme => ({
@@ -11,24 +14,33 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+interface IIntentionList {
+  intentions: IIntention[];
+}
 
 const CardList = (props: any) => {
   const classes = useStyles();
+  const [ { intentions }, setState ] = useState<IIntentionList>({ intentions: []});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://localhost:3001/intentions',
+      );
+  
+      setState(result.data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <Container className={classes.cardGrid} maxWidth="md">
       {/* End hero unit */}
       <Grid container={true} spacing={4}>
-        {cards.map(card => (
-          <Grid item={true} key={card} xs={12} sm={6} md={4}>
+        {intentions.map(intention => (
+          <Grid item={true} key={intention.id} xs={12} sm={6} md={4}>
             <Intention
-              intention={{
-                description: "Przyłącz się do modlitwy w intencji....",
-                id: "#",
-                title: "Modlitwa w intencji ....",
-                userId: "#"
-              }}
+              intention={intention}
             />
           </Grid>
         ))}
