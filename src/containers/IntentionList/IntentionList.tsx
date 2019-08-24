2e13dfe53d2cfe25fc550934fd6a6a6d86522c60
Grzegorz Motyl <grzegorz.motyl@stepstone.com>
2019-08-24 13:15:27 +0200

    #2 custom hook for rosary api added

--- src/containers/IntentionList/IntentionList.tsx
+++ src/containers/IntentionList/IntentionList.tsx
@@ -1,11 +1,10 @@
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-import axios from 'axios';
 import * as React from "react";
-import { useEffect, useState } from 'react';
 import { IIntention } from 'src/components/Intention/Interface';
 import Intention from "../../components/Intention";
+import useRosaryApi from './api';
 
 const useStyles = makeStyles(theme => ({
   cardGrid: {
@@ -20,24 +19,14 @@ interface IIntentionList {
 
 const CardList = (props: any) => {
   const classes = useStyles();
-  const [ { intentions }, setState ] = useState<IIntentionList>({ intentions: []});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        'http://localhost:3001/intentions',
-      );
-  
-      setState(result.data);
-    };
-    fetchData();
-  }, []);
+  const { state } = useRosaryApi<IIntentionList>('intentions', { intentions: []});
+  const { data } = state;
 
   return (
     <Container className={classes.cardGrid} maxWidth="md">
       {/* End hero unit */}
       <Grid container={true} spacing={4}>
-        {intentions.map(intention => (
+        {data.intentions.map(intention => (
           <Grid item={true} key={intention.id} xs={12} sm={6} md={4}>
             <Intention
               intention={intention}
---
+++ src/containers/IntentionList/api.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const useRosaryApi = <T>(endpoint: string, initialData: T) => {
+    const [data, setData] = useState(initialData);
+    const [url, setUrl] = useState(
+      `http://localhost:3001/${endpoint}`,
+    );
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const doFetch = (query: string) => setUrl(`http://localhost:3001/${endpoint}?${query}`);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        setIsError(false);
+        setIsLoading(true);
+  
+        try {
+          const result = await axios(url);
+  
+          setData(result.data);
+        } catch (error) {
+          setIsError(true);
+        }
+  
+        setIsLoading(false);
+      };
+  
+      fetchData();
+    }, [url]);
+  
+    return { state: { data, isLoading, isError }, doFetch };
+  }
+
+  export default useRosaryApi;
