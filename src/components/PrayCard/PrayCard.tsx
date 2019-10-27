1974afe8a39b38ea30a43203e4cc26f887352213
Grzegorz Motyl <grzegorz.motyl@stepstone.com>
2019-10-27 20:34:31 +0100

    #6 added imgages to pray card

--- src/components/PrayCard/PrayCard.tsx
+++ src/components/PrayCard/PrayCard.tsx
@@ -1,15 +1,13 @@
+import { Grid } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import * as React from "react";
 import { IMystery } from 'src/consts/rosary';
-
-const image =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs0Q8FdKF0hQMurDCnu0sR9A7yUIZ6lgddZREsY_xbtWz5b_HN9w";
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -21,13 +19,24 @@ const useStyles = makeStyles(theme => ({
   cardContent: {
     flexGrow: 1
   },
-  cardMedia: {
-    paddingTop: "56.25%" // 16:9
-  }
+  bigAvatar: {
+    margin: 10,
+    width: 200,
+    height: 200,
+  },
 }));
+
+interface IPrayCard {
+  isPraying: boolean;
+  mystery: IMystery;
+  onPrayRequestAction: () => void;
+  onPrayAction: () => void;
+}
+
 
 const PrayCard: React.ComponentType<IPrayCard> = (props) => {
   const { isPraying, mystery } = props;
+  const image = `/img/${mystery.type}.jpg`
   const classes = useStyles();
   const actions =
     <CardActions>
@@ -42,11 +51,9 @@ const PrayCard: React.ComponentType<IPrayCard> = (props) => {
 
   return (
     <Card className={classes.card}>
-      <CardMedia
-        className={classes.cardMedia}
-        image={image}
-        title="Image title"
-      />
+      <Grid container={true} justify="center" alignItems="center">
+        <Avatar alt="loading..." src={image} className={classes.bigAvatar} />
+      </Grid>
       <CardContent className={classes.cardContent}>
         <Typography gutterBottom={true} variant="h5" component="h2">
           { isPraying 
@@ -66,44 +73,5 @@ const PrayCard: React.ComponentType<IPrayCard> = (props) => {
     </Card>
   );
 };
-
-interface IPrayCard {
-  isPraying: boolean;
-  mystery: IMystery;
-  onPrayRequestAction: () => void;
-  onPrayAction: () => void;
-}
-
-// const PrayCard: React.ComponentType<IPrayCard> = (props) => {
-//   const { isPraying, mystery } = props;
-//   return (
-//     <Grid container={true} spacing={2}>
-//       <Grid item={true} xs={12}>
-//         { isPraying
-//             ?  <>
-//                 <Paper>{mystery.title}</Paper>
-//                 <Paper>{mystery.description}</Paper>
-//               </>
-//             :  null
-//         }
-//       </Grid>
-//       <Divider />
-//       <Grid item={true} xs={12}>
-//         {/* TODO display pondering of mystery */}
-//       </Grid>
-//       <Divider />
-//       <Grid item={true} xs={6}>
-        // <Button size="small" color="primary" onClick={props.onPrayRequestAction} disabled={isPraying}>
-        //   Pobierz tajemnicę
-        // </Button>
-//       </Grid>
-//       <Grid item={true} xs={6}>
-//         <Button size="small" color="primary" onClick={props.onPrayAction} disabled={!isPraying}>
-//           Gotowe (zapisz)
-//         </Button>
-//       </Grid>
-//     </Grid>
-//   );
-// };
 
 export default PrayCard;
