c3884a3a10ab562a72d3e14d38ff0f900c9d8ddb
Grzegorz Motyl <grzegorz.motyl@stepstone.com>
2020-02-29 13:51:01 +0100

    fix(mystery): use enum in mystery type test

--- src/consts/__tests__/rosary.test.ts
+++ src/consts/__tests__/rosary.test.ts
@@ -1,11 +1,16 @@
 import {getMystery} from '../rosary'
+import {MysteryTypes} from '../MysteryTypes'
 
-test('Rosary helper', () => {
-  const mystery1 = getMystery(1)
-  const mystery7 = getMystery(7)
-  const mystery18 = getMystery(18)
+test('Rosary helper returns correct mystery by type', () => {
+  const mystery1 = getMystery(MysteryTypes.Joyful1)
+  const mystery7 = getMystery(MysteryTypes.Luminous2)
+  const mystery18 = getMystery(MysteryTypes.Glorious3)
+  const mystery0 = getMystery(MysteryTypes.none)
+  const mystery21 = getMystery(21)
 
   expect(mystery1.title).toMatch('Zwiastowanie')
   expect(mystery7.title).toMatch('Objawienie')
-  expect(mystery18.title).toMatch('Zesłanie Ducha Świętegoexit')
+  expect(mystery18.title).toMatch('Zesłanie Ducha Świętego')
+  expect(mystery0.title).toMatch('loading...')
+  expect(mystery21.title).toMatch('loading...')
 })
