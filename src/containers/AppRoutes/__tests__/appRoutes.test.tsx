32e38bbfb3889fe0b97ad77adbd8956d0d7bbb2d
Grzegorz Motyl <grzegorz.motyl@stepstone.com>
2020-06-08 10:00:18 +0200

    feat(#9): Add link to add intention button

--- src/containers/AppRoutes/__tests__/appRoutes.test.tsx
+++ src/containers/AppRoutes/__tests__/appRoutes.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import {AppRoutes} from '../AppRoutes'
 import {Router} from 'react-router-dom'
 import {createMemoryHistory} from 'history'
 import {render, fireEvent, act} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
+import {AppRoutes} from '../AppRoutes'
+import Hero from '../../../components/Hero'
+
 jest.mock('../../IntentionList', () => () => <div>Intention list</div>)
+jest.mock('../../LoginPage', () => () => <div>Login page</div>)
 
 it('should open login form on login link click', () => {
   const history = createMemoryHistory()
@@ -16,4 +19,20 @@ it('should open login form on login link click', () => {
   )
 
   expect(container.innerHTML).toMatch('Intention list')
+})
+
+it('For not logged user: should open login form on add intention button click', () => {
+  const history = createMemoryHistory()
+  const {container, getByText, getByTestId} = render(
+    <Router history={history}>
+      <Hero />
+      <AppRoutes />
+    </Router>,
+  )
+
+  expect(container.innerHTML).toMatch('Intention list')
+
+  fireEvent.click(getByTestId('add-intention'))
+
+  expect(container.innerHTML).toMatch('Login page')
 })
