2a20975d1126aa46678303a623c02a7234d9e9d6
Grzegorz Motyl <grzegorz.motyl@stepstone.com>
2020-10-09 11:10:40 +0200

    test(#30): login page test

--- src/containers/LoginPage/__tests__/loginPage.test.jsx
+++ src/containers/LoginPage/__tests__/loginPage.test.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import LoginCard from '../LoginCard'
-import {MemoryRouter as Router} from 'react-router-dom'
+import LoginPage from '../LoginPage'
+import {renderWithRouter} from 'src/tools/renderWithRouter'
 
 const mockRequest = jest.fn()
 
-jest.mock('../../../hooks/useRosaryApi', () => ({
+jest.mock('src/hooks/useRosaryApi', () => ({
   useAuthTokenRequest: () => ({
     state: {isLoading: false, data: {token: ''}},
     requestAuthToken: mockRequest,
@@ -14,25 +14,19 @@ jest.mock('../../../hooks/useRosaryApi', () => ({
 }))
 
 test('calls submit with the username and password when submitted', async () => {
-  // const container = document.createElement('div')
-  // const mockSubmit = jest.fn()
-  // ReactDOM.render(
-  //   <Router>
-  //     <LoginCard error={{isError: false}} handleSubmit={mockSubmit} />
-  //   </Router>,
-  //   container,
-  // )
-  // const form = container.querySelector('form')
-  // const {email, password} = form.elements
-  // const submit = new Event('submit')
-  // email.value = 'test@test.pl'
-  // password.value = 'secret'
-  // form.dispatchEvent(submit)
-  // expect(mockRequest).toHaveBeenCalledTimes(1)
-  // expect(mockRequest).toHaveBeenCalledWith({
-  //   email: 'test@test.pl',
-  //   password: 'secret',
-  // })
+  const mockSubmit = jest.fn()
+  const {container} = renderWithRouter(<LoginPage />)
+  const form = container.querySelector('form')
+  const {email, password} = form.elements
+  const submit = new Event('submit')
+  email.value = 'test@test.pl'
+  password.value = 'secret'
+  form.dispatchEvent(submit)
+  expect(mockRequest).toHaveBeenCalledTimes(1)
+  expect(mockRequest).toHaveBeenCalledWith({
+    email: 'test@test.pl',
+    password: 'secret',
+  })
 })
 
 // shows error message
